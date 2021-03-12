f47666d7e683dffae0e96c54bbefce5998f6aef9
NataliaMelnik <bukato.natallia@gmail.com>
2021-03-12 23:06:39 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (matrix === undefined || matrix === null) {
+    return [];
+  }
+  let result = matrix.map((item, index) => {
+    if (index % 2 === 0) {
+      return item;
+    } else {
+      return item.reverse();
+    }
+  });
+  return result.flat(Infinity);
 }
